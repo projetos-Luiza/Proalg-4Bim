cfb2e7c30b52430c65908d98cf62a03b3bc01bae
Luiza Pereira Silva <80282273+projetos-Luiza@users.noreply.github.com>
2021-10-25 23:18:40 -0300

    Add files via upload

---
+++ Aula 1 - variaveis/variaveis.js
@@ -0,0 +1,5 @@
+var num = 10;
+var num2 = 200;
+var texto = "Campanha Arrecadação";
+var cliente = "Luiza Silva";
+pontos = num + num2;
---
+++ Aula 2 - entrada de dados e funções/entrada.js
@@ -0,0 +1,7 @@
+function pedirvalores (){
+		num1 = parseInt(prompt("Digite o primeiro número"));
+		num2 = parseInt(prompt("Digite o segundo número"));
+	
+		soma = num1+num2
+		subtracao = num1-num2
+	}
